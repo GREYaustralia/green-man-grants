8f064d171ea489e6fed5c68f041ad0df240e7dce
Nathan Micic <nathan.micic@gmail.com>
2016-07-21 11:56:04 +1000

    Committing changes

--- assets/js/entries.js
+++ assets/js/entries.js
@@ -27,13 +27,14 @@
 //   })
 // })
 $(document).ready(function(){
-  $('.btn').hide();
+  $('.entries-box').find('.btn').hide();
   $('.box').on('mouseover', function(){
     if($('.box').hasClass('video')){
         $(this).find('.icon').hide()
         $(this).find('.btn').toggle().show()
         $(this).find('.box').css('border', '2px solid green')
         $(this).find('.greenoverlay').css('background-color', '#45ffb7')
+        $(this).find('p').css('margin-top', '10vh')
         // $($('#top').children()[1]).whatever()
     }else if($('.box').hasClass('text')){
         $(this).find('.icon').hide()
@@ -54,6 +55,7 @@ $(document).ready(function(){
   }).on('mouseout', function(){
         $('.btn').hide();
         $('.greenoverlay').css('background-color', '#14523a')
+        $(this).find('p').css('margin-top', '30vh')
         $('.icon').show()
   })
 })
