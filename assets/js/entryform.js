f07e9f6b335937e68e2c8d59d86b2849305e54a7
Nathan Micic <nathan.micic@gmail.com>
2016-08-01 05:37:48 +1000

    Committing weekend changes   - Responsibility with any screen size   - Modal added   - New Copy   - reworked index   - reworked briefs   - briefs indicator added   - footer added each page (need workings)   - reworked forms   - added bigger checkboxes   - +++

--- assets/js/entryform.js
+++ assets/js/entryform.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
   $('#upload').change(function(){
     if ($(this).find(':selected').val() === 'text'){
-      $('#entry-form').css('height', '155vh');
-      $('.dotoverlay').css('height', '155vh');
+      // $('#entry-form').css('height', '155vh');
+      // $('.dotoverlay').css('height', '155vh');
       // $(function() {
       //   $(document).scrollTop( $("#form-upload").offset().top + (-75) );
       // });
       $('#upload-field').empty();
       $('#upload-field').append('<textarea class="large-12 columns" id="text-submission" name="text-submission" placeholder="IDEA DESCRIPTION (MAX 1000 CHARACTERS LINKS OK)"></textarea>')
     } else if($(this).find(':selected').val() === 'video'){
-      $('#entry-form').css('height', '155vh');
-      $('.dotoverlay').css('height', '155vh');
+      // $('#entry-form').css('height', '155vh');
+      // $('.dotoverlay').css('height', '155vh');
       $(function() {
         $(document).scrollTop( $("#form-upload").offset().top + (-75) );
       });
@@ -21,8 +21,8 @@ $(document).ready(function(){
       // $('#upload-field').append('<progress></progress>')
 
     }else if($(this).find(':selected').val() === 'audio'){
-      $('#entry-form').css('height', '155vh');
-      $('.dotoverlay').css('height', '155vh');
+      // $('#entry-form').css('height', '155vh');
+      // $('.dotoverlay').css('height', '155vh');
       $(function() {
         $(document).scrollTop( $("#form-upload").offset().top + (-75) );
       });
@@ -30,8 +30,8 @@ $(document).ready(function(){
       $('#upload-field').append('<input name="file" type="file"/>');
       $('#upload-field').append('<textarea class="large-12 columns" id="description" name="description" placeholder="AUDIO DESCRIPTION (MAX 1000 CHARACTERS LINKS OK)"></textarea>')
     }else if($(this).find(':selected').val() === 'photo'){
-      $('#entry-form').css('height', '155vh');
-      $('.dotoverlay').css('height', '155vh');
+      // $('#entry-form').css('height', '155vh');
+      // $('.dotoverlay').css('height', '155vh');
       $(function() {
         $(document).scrollTop( $("#form-upload").offset().top + (-75) );
       });
@@ -40,8 +40,8 @@ $(document).ready(function(){
       $('#upload-field').append('<textarea class="large-12 columns" id="description" name="description" placeholder="PHOTO DESCRIPTION (MAX 1000 CHARACTERS LINKS OK)"></textarea>')
     }else{
       $('#upload-field').empty();
-      $('#entry-form').css('height', '100vh');
-      $('.dotoverlay').css('height', '100vh');
+      // $('#entry-form').css('height', '100vh');
+      // $('.dotoverlay').css('height', '100vh');
       $(document).scrollTop();
     }
   });
