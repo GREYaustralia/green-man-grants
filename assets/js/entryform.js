6f60c1cfe05bcc6b654be3da6960c1b08889d748
Nathan Micic <nathan.micic@gmail.com>
2016-08-09 11:24:20 +1000

    pushing accessibility changes

--- assets/js/entryform.js
+++ assets/js/entryform.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
+  console.log('ready')
   $('#brief-question').change(function(){
-    if ($(this).find(':selected').val() === 'vehiclesafety'){
+    console.log('change')
+    if ($(this).find(':selected').val() === '1'){
       $('#question').empty();
       $('#question').text('How would you get your peers to understand the importance of buying a safe affordable car?');
-    } else if($(this).find(':selected').val() === 'vulnerability'){
+    } else if($(this).find(':selected').val() === '2'){
       $('#question').empty();
       $('#question').text('The human body has not evolved as fast as cars have, therefore we are very vulnerable when on the roads. How would you highlight this to your peers to help reduce risk taking behaviour?');
-    } else if($(this).find(':selected').val() === 'sharedresponibility'){
+    } else if($(this).find(':selected').val() === '3'){
       $('#question').empty();
       $('#question').text('How would you convince your peers to believe that zero deaths and serious injuries on our roads could become a reality?');
     }else{
@@ -17,24 +19,24 @@ $(document).ready(function(){
 
 
   $('#upload').change(function(){
-    if ($(this).find(':selected').val() === 'text'){
+    if ($(this).find(':selected').val() === '1'){
       $('#upload-field').empty();
       $('#upload-field').append('<textarea class="large-12 columns" id="text-submission" name="text-submission" placeholder="IDEA DESCRIPTION (MAX 1000 CHARACTERS LINKS OK)" aria-label="Text Entry Field - Max 1000 Characters" required></textarea>')
-    } else if($(this).find(':selected').val() === 'video'){
+    } else if($(this).find(':selected').val() === '3'){
       $(function() {
         $(document).scrollTop( $("#form-upload").offset().top + (-75) );
       });
       $('#upload-field').empty();
       $('#upload-field').append('<input name="file" type="file" aria-label="Video Upload" required/>');
-      $('#upload-field').append('<textarea class="large-12 columns" id="description" name="description" placeholder="VIDEO DESCRIPTION (MAX 1000 CHARACTERS LINKS OK)" aria-label="Video Description - Max 140 Characters" required></textarea>')
-    }else if($(this).find(':selected').val() === 'audio'){
+      $('#upload-field').append('<textarea class="large-12 columns" id="description" name="description" placeholder="VIDEO DESCRIPTION (MAX 140 CHARACTERS LINKS OK)" aria-label="Video Description - Max 140 Characters" required></textarea>')
+    }else if($(this).find(':selected').val() === '2'){
       $(function() {
         $(document).scrollTop( $("#form-upload").offset().top + (-75) );
       });
       $('#upload-field').empty();
       $('#upload-field').append('<input name="file" type="file" aria-label="Audio Upload" required/>');
       $('#upload-field').append('<textarea class="large-12 columns" id="description" name="description" placeholder="AUDIO DESCRIPTION (MAX 140 CHARACTERS LINKS OK)" aria-label="Audio Description - Max 140 Characters" required></textarea>')
-    }else if($(this).find(':selected').val() === 'photo'){
+    }else if($(this).find(':selected').val() === '4'){
       $(function() {
         $(document).scrollTop( $("#form-upload").offset().top + (-75) );
       });
